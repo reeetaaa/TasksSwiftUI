c2c9e064cffea8ff003209d8318b424a1ed8e023
reeetaaa <ritababu2000@yahoo.com>
2024-04-22 21:08:22 +0300

    Initial Commit

---
+++ TasksList/TasksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksListApp.swift
+//  TasksList
+//
+//  Created by Маргарита Бабухадия on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TasksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
