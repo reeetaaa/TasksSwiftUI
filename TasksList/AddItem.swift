d7af235e42adae6b128120a293f5274152f45d83
reeetaaa <ritababu2000@yahoo.com>
2024-04-23 18:07:47 +0300

    first commit

---
+++ TasksList/AddItem.swift
@@ -0,0 +1,37 @@
+//
+//  AddItem.swift
+//  TasksList
+//
+//  Created by Маргарита Бабухадия on 22.04.2024.
+//
+
+import SwiftUI
+
+struct AddItem: View {
+    @ObservedObject var tasks = ListItem()
+    @State private var task = ""
+    @State private var description = ""
+    
+    @Environment (\.presentationMode) var presentationMode
+    
+    var body: (some View) {
+        NavigationView {
+            Form {
+                TextField("Задача", text: $task)
+                TextField("Описание задачи", text: $description)
+                
+            }   .navigationBarTitle("Новая задача")
+                .navigationBarItems(trailing:
+                    Button("Добавить задачу"){
+                            let i = Item(task: self.task, description: self.description, isDone: false)
+                            self.tasks.items.append(i)
+                            self.presentationMode.wrappedValue.dismiss()
+                    }.foregroundColor(.green)
+                )
+        }
+    }
+}
+
+#Preview {
+    AddItem()
+}
--- TasksList/ContentView.swift
+++ TasksList/ContentView.swift
@@ -7,15 +7,68 @@
 
 import SwiftUI
 
+struct Item: Identifiable, Hashable {
+    var id = UUID()
+    var task: String
+    var description: String
+    var isDone: Bool
+}
+
+class ListItem: ObservableObject {
+    @Published var items = [Item]()
+}
+
 struct ContentView: View {
+    @ObservedObject var tasks = ListItem()
+    @State private var showingAdd = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack(alignment: .leading) {
+                Text("Задач на сегодня: \(tasks.items.filter { $0.isDone == false }.count)")
+                    .padding(.leading)
+                List {
+                    ForEach(Array(tasks.items.enumerated()), id: \.offset) { i, e in
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text("Задача №\(i+1)")
+                                    
+                                Text(e.task)
+                                    .font(.title2)
+                                    .bold()
+                                Text(e.description)
+                                    .font(.footnote)
+                            }
+                            Spacer()
+                            Button {
+                                tasks.items[i].isDone.toggle()
+                            } label: {
+                                Image (systemName:
+                                        e.isDone ? "checkmark.circle.fill" : "circle")
+                                .foregroundColor(.green)
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                }
+                .navigationTitle("Список дел на день")
+                .navigationBarItems(trailing:
+                                        Button(action:{
+                    self.showingAdd = true
+                }) {
+                    Image (systemName: "plus")
+                        .foregroundColor(.green)
+                        .font(.title2)
+                    
+                }) .sheet(isPresented: $showingAdd) {
+                    AddItem(tasks: self.tasks)
+                }
+            }
         }
-        .padding()
+    }
+    
+    func removeItems(as offsets: IndexSet) {
+        tasks.items.remove(atOffsets: offsets)
     }
 }
 
